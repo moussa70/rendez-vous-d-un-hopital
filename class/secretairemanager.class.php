ab197ed89d2986a7143a86fb5aaf3383374d3370
moussa70 <Papemoussaseck70@gmail.com>
2019-10-27 18:05:06 +0000

    ajouter

--- class/secretairemanager.class.php
+++ class/secretairemanager.class.php
@@ -1,19 +1,21 @@
 <?php
+require "../class/bd.class.php";
     class SecretaireManager extends Mysql
     {
         public function add(Secretaire $objet){
             
             $pdo = Mysql::connect();
-            $query = $pdo->prepare("INSERT INTO utilisateur(nom, prenom, passwd, username, email, telephone, id_role) VALUES(:nom, :prenom, :passwd, :email, :telephone, :username, :id_role)");
+            $query = $pdo->prepare("INSERT INTO utilisateur(nom, prenom, passwd, username, email, telephone, id_role) VALUES(:nom, :prenom, :passwd, :username, :email, :telephone, :id_role)");
             
             $query->bindValue(':nom', $objet->getnom());
             $query->bindValue(':prenom', $objet->getprenom());
             $query->bindValue(':passwd', $objet->getpasswd());
-            $query->bindValue(':telephone', $objet->gettelephonee());
+            $query->bindValue(':telephone', $objet->gettelephone());
             $query->bindValue(':email', $objet->getemail());
             $query->bindValue(':username', $objet->getusername());
             $query->bindValue(':id_role', $objet->getid_role());
             $query->execute();
+          
         }
 
     }
--- page/adminserver.php
+++ page/adminserver.php
@@ -1,28 +1,25 @@
 <?php
 if(isset($_POST['save'])) {
-require "../class/bd.class.php";
-require "../class/secretaire.php";
-require "../class/secretairemanager.php";
+require "../class/secretaire.class.php";
+require "../class/secretairemanager.class.php";
 if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['username']) && !empty($_POST['passwd']))
 {
     $nom=$_POST['nom'];
     $prenom=$_POST['prenom'];
     $username=$_POST['username'];
     $passwd=$_POST['passwd'];
-}
-echo 'ok';
- $perso = new Secretaire();
+
+$perso = new Secretaire();
  $perso->hydrate([
-     "nom" => $nom,
-     "prenom" => $prenom,
-     "username" => $username,
-     "passwd" => $passwd,
-     "telephone" => $telephone,
-     "email" => $email,
-     ]);
-     echo "ok";
- $moussa = new SecretaireManager();
- $moussa->add($perso);
- header("Location: admin.php");
+   "nom" => $nom,
+    "prenom" => $prenom,
+    "username" => $username,
+   "passwd" => $passwd
+   ]);
+$moussa= new SecretaireManager();
+$moussa->add($perso);
+header("Location: admin.php");
+
+}
 }
 
